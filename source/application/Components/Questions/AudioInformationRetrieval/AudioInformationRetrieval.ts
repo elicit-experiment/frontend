9df840621197b99c0e1ee1bd59ef4e50e1bd96ba
Jacob Poul Richardt <jacob@poulrichardt.dk>
2016-11-17 12:47:52 +0100

    Added timelabels

--- source/application/Components/Questions/AudioInformationRetrieval/AudioInformationRetrieval.ts
+++ source/application/Components/Questions/AudioInformationRetrieval/AudioInformationRetrieval.ts
@@ -4,9 +4,9 @@ import QuestionModel = require("Models/Question");
 import AudioInfo = require("Components/Players/Audio/AudioInfo");
 
 type Selection = {Identifier:string};
-
 type Segment = {Title:string, Start:number, End:number, Length:number};
 type Channel = {Title:string, Segments:Segment[], TrackElement:KnockoutObservable<HTMLElement>};
+type TimeSegment = {Text:string, Position:number};
 
 class AudioInformationRetrieval extends QuestionBase<{Selections:Selection[]}>
 {
@@ -15,6 +15,7 @@ class AudioInformationRetrieval extends QuestionBase<{Selections:Selection[]}>
 	public ZoomLevel = knockout.observable(1);
 	public TracksElement = knockout.observable<HTMLElement>(null);
 	public TracksLength = knockout.observable<number>();
+	public TimeSegments = knockout.observableArray<TimeSegment>();
 
 	public Channels = knockout.observableArray<Channel>();
 
@@ -22,14 +23,16 @@ class AudioInformationRetrieval extends QuestionBase<{Selections:Selection[]}>
 	{
 		super(question);
 
-		var searchView = this.GetInstrument("SearchView");
+		let searchView = this.GetInstrument("SearchView");
 
 		this.SearchViewHeader(searchView["Header"]["Label"]);
 		this.SearchViewButtonLabel(searchView["Button"]["Label"]);
 
-		this.Channels.push(this.CreateChannel("Taler"), this.CreateChannel("Transkriptioner"))
+		this.TracksLength(80000);
 
-		this.UpdateTracksLength();
+		this.Channels.push(this.CreateChannel("Taler"), this.CreateChannel("Transkriptioner"));
+
+		this.AddTimeSegments();
 	}
 
 	public Search():void
@@ -47,7 +50,7 @@ class AudioInformationRetrieval extends QuestionBase<{Selections:Selection[]}>
 	}
 	
 	public CreateSegments():Segment[] {
-		var segments:Segment[] = [];
+		let segments:Segment[] = [];
 
 		for (let i = 0; i < 1000; i++)
 			segments.push(this.CreateSegment("Segment " + i, i * 80, i * 80 + 50));
@@ -57,7 +60,7 @@ class AudioInformationRetrieval extends QuestionBase<{Selections:Selection[]}>
 
 	public ZoomTracks(viewModel:any, event:JQueryMouseEventObject):void
 	{
-		var originalEvent = (<WheelEvent>(<any>event).originalEvent);
+		let originalEvent = (<WheelEvent>(<any>event).originalEvent);
 
 		this.ZoomLevel(this.ZoomLevel() * (originalEvent.deltaY > 0 ? 1.1 : 0.9));
 
@@ -69,24 +72,27 @@ class AudioInformationRetrieval extends QuestionBase<{Selections:Selection[]}>
 		});
 	}
 
-	private UpdateTracksLength():void
+	private AddTimeSegments():void
 	{
-		var length = 0;
-		for(var channel of this.Channels())
-		{
-			for(var segment of channel.Segments)
-			{
-				if(segment.End > length)
-					length = segment.End;
-			}
-		}
+		this.TimeSegments.removeAll();
 
-		this.TracksLength(length);
+		for(let i = 0; i < this.TracksLength(); i += 1000)
+		{
+			this.TimeSegments.push(this.CreateTimeSegment(i));
+		}
+	}
+
+	private CreateTimeSegment(position:number):TimeSegment
+	{
+		return {
+			Text: this.GetTimeCode(position),
+			Position: position
+		};
 	}
 
 	private CreateSegment(title:string, start:number, end:number):Segment
 	{
-		var result:Segment = {
+		let result:Segment = {
 			Title: title,
 			Start: start,
 			End: end,
@@ -94,6 +100,21 @@ class AudioInformationRetrieval extends QuestionBase<{Selections:Selection[]}>
 		};
 
 		return result;
+	}
+
+	private GetTimeCode(position: number):string
+	{
+		let hours = Math.floor(position / (60 * 60 * 1000));
+		let minutes = Math.floor((position % (60 * 60 * 1000)) / (60 * 1000));
+		let seconds = Math.floor((position % (60 * 1000)) / 1000);
+		let milliseconds = position % 1000;
+
+		return `${hours}:${this.ToTwoDigits(minutes)}:${this.ToTwoDigits(seconds)}.${milliseconds}`;
+	}
+
+	private ToTwoDigits(value:number):string
+	{
+		return value < 10 ? "0" + value : value.toString();
 	}
 }
 
