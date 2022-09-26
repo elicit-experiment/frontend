3a94f35d27a82b4f0227f95d15072fed19fe2c77
Iain Bryson <bryson.iain@gmail.com>
2022-09-26 06:17:55 -1000

    Add mouse move monitor

--- source/application/Components/Questions/Monitor/Monitor.ts
+++ source/application/Components/Questions/Monitor/Monitor.ts
@@ -1,8 +1,10 @@
 ﻿import QuestionBase = require('Components/Questions/QuestionBase');
 import QuestionModel = require('Models/Question');
+import mouseTrackingManager = require('Managers/MouseTrackingManager');
 
 class Monitor extends QuestionBase<{ Context: { Type: string; Data: string }; TimeZone: { Offset: number } }> {
   public KeyDownHandler: { (event: KeyboardEvent): void } | null = null;
+  public MouseMoveHandler: { (event: MouseEvent): void } | null = null;
 
   constructor(question: QuestionModel) {
     super(question, false);
@@ -17,12 +19,23 @@ class Monitor extends QuestionBase<{ Context: { Type: string; Data: string }; Ti
       this.AddEvent('KeyDown', 'None', event.code.toString());
     };
     document.addEventListener('keydown', this.KeyDownHandler);
+
+    mouseTrackingManager.StartTracking();
+    this.MouseMoveHandler = (event: MouseEvent) => {
+      mouseTrackingManager.ProcessPoint(
+        { x: event.x, y: event.y, timeStamp: new Date().getTime() },
+        new Date().getTime(),
+      );
+    };
+    document.addEventListener('mousemove', this.MouseMoveHandler);
   }
 
   public SlideCompleted(): boolean {
     this.AddEvent('Stop', 'Monitor');
 
     this.UpdateAnswer();
+
+    mouseTrackingManager.StopTracking();
 
     return true;
   }
